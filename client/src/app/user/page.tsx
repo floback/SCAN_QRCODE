8feee7330fbd9e0ef1a9f9b7b9e42060fe20c639
Filipe Loback <filipelobacksiqueiraoperaciona@gmail.com>
2025-07-17 10:26:49 -0300

    add style

--- client/src/app/user/page.tsx
+++ client/src/app/user/page.tsx
@@ -66,8 +66,8 @@ export default function UserManagementPage() {
                 <Search className="absolute left-3 top-3 h-5 w-5 text-gray-800" />
                 <input
                 type="text"
-                placeholder="Buscar por usuário"
-                className="pl-10 pr-4 py-2 border-2 border-gray-400 rounded-full text-base shadow-md bg-white text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Search"
+                className="pl-10 pr-4 py-2 border-2 border-gray-200 rounded-md text-base shadow-md bg-white text-gray-600 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
                 />
